ffb239fcf2f7260a701f68d364dd1da1b82a49b7
marianitadn <maria.nita.dn@gmail.com>
2013-11-23 15:05:41 +0200

    Fix indent

--- berry_server/models.py
+++ berry_server/models.py
@@ -3,7 +3,7 @@ from sqlalchemy import Column, Integer, String
 from database import Base
 
 class User(Base):
-        __tablename__ = 'users'
+    __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True)
     username = Column(String(50), unique=True)
