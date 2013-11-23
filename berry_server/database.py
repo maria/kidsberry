32a17e0a90b1c48ecc59648ff6050c612208f6e6
marianitadn <maria.nita.dn@gmail.com>
2013-11-23 14:45:55 +0200

    Add dropbox access token to User model.

--- berry_server/database.py
+++ berry_server/database.py
@@ -1,8 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from settings_local import DATABASE_URI
 
-engine = create_engine('sqlite:////tmp/kidsberry.db', convert_unicode=True)
+engine = create_engine(DATABASE_URI, convert_unicode=True)
 db_session = scoped_session(sessionmaker(autocommit=True, autoflush=True,
                                          bind=engine))
 Base = declarative_base()
