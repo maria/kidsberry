e4c3523177d862738b5f8bd34dfe143b5d008232
Ada Solcan <ada.solcan@gmail.com>
2013-11-23 15:43:42 +0200

    added CameraVideo class

---
+++ berry_server/camera_video.py
@@ -0,0 +1,8 @@
+from SimpleCV import Camera, VideoStream, Color, Display
+from time import sleep
+
+myCamera = Camera(prop_set={'width': 320, 'height': 240})
+
+class CameraVideo:
+	def 
+myDisplay = Display(resolution=(320, 240))
