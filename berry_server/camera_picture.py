9d7969e5084429eb42d9ff4921ca88e19d72b75e
Ada Solcan <ada.solcan@gmail.com>
2013-11-23 15:44:23 +0200

    modified camera picture class

--- berry_server/camera_picture.py
+++ berry_server/camera_picture.py
@@ -4,13 +4,13 @@ from datetime import datetime
 
 class CameraPicture(object):
     def __init__(self):
-	self.my_camera = Camera(prop_set={'width':320, 'height': 240})
-	self.timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
+        self.my_camera = Camera(prop_set={'width':320, 'height': 240})
+        self.timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
 
 
     def take_picture(self):
-	frame = self.my_camera.getImage()
-	file_name = "cameraOut" + self.timestamp + ".jpg"
-	frame.save(file_name)
-	return file_name
+        frame = self.my_camera.getImage()
+        file_name = "cameraOut" + self.timestamp + ".jpg"
+        frame.save(file_name)
+        return file_name
 
