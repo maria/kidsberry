fdfc6fdacc64b7b65237cbc919bcc1b939fd7812
Ada Solcan <ada.solcan@gmail.com>
2013-11-24 08:37:38 +0200

    added live preview

--- berry_server/camera_video.py
+++ berry_server/camera_video.py
@@ -1,12 +1,13 @@
 from datetime import datetime
 from subprocess import call
 from SimpleCV import Camera, VideoStream, Color, Display
-import time
+from time import sleep
 
 class CameraVideo(object):
 
     def __init__(self):
         self.my_camera = Camera(prop_set={'width': 320, 'height': 240})
+        self.my_display = Display(resolution=(320, 240))
         self.live_preview = False
         self.timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
 
@@ -16,22 +17,22 @@ class CameraVideo(object):
             self.started_live_preview = True
             video_stream = VideoStream(self.file_name, fps=15)
         framecount = 0
-        while(framecount < 15*60):
+        while (not myDisplay.isDone()) and (self.live_preview is True):
         #while self.live_preview is True:
             #image = my_camera.getImage()
             #image = image.edges()
             #video_stream.writeFrame(image)
-            self.my_camera.getImage().save(video_stream)
+            self.my_camera.getImage().save(self.my_display)
             time.sleep(0.1)
 
 
     def stop_live_preview(self):
         self.live_preview = False
         # construct the encoding arguments
-        outname = self.file_name.replace('.avi', '.mp4')
-        params = " -i {0} {1}".format(self.file_name, outname)
+        #outname = self.file_name.replace('.avi', '.mp4')
+        #params = " -i {0} {1}".format(self.file_name, outname)
         # run ffmpeg to compress your video.
-        call('ffmpeg' + params, shell=True)
+        #call('ffmpeg' + params, shell=True)
         return outname
 
     def take_video(self, duration):
