0e6ea15f1b26bba966ca5cc8f619f2c0b0aba92e
Maria Nita <maria.nita.dn@gmail.com>
2013-11-24 07:31:17 +0000

    check if camera open

--- berry_server/camera_picture.py
+++ berry_server/camera_picture.py
@@ -6,10 +6,12 @@ from SimpleCV import Camera
 
 class CameraPicture(object):
 
-    def __init__(self):
-        self.my_camera = Camera(prop_set={'width':320, 'height': 240})
+    #def __init__(self):
+        #self.my_camera = Camera(prop_set={'width':320, 'height': 240})
 
     def take_picture(self):
+	if hasattr(self, 'my_camera') is False:
+	    self.my_camera = Camera(prop_set={'width':320, 'height': 240})
         frame = self.my_camera.getImage()
         self.timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
         file_name = "/tmp/cameraOut" + self.timestamp + ".jpg"
